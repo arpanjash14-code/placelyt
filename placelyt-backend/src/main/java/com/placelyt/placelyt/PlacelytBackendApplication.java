a6247904c41606214ca273e56ca833ff7634746a
Arpan Jash <arpanjash14@gmail.com>
2026-09-02 11:50:25 +0530

    Add Spring Boot backend setup

---
+++ placelyt-backend/src/main/java/com/placelyt/placelyt/PlacelytBackendApplication.java
@@ -0,0 +1,13 @@
+package com.placelyt.placelyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacelytBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacelytBackendApplication.class, args);
+	}
+
+}
---
+++ placelyt-backend/src/test/java/com/placelyt/placelyt/PlacelytBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placelyt.placelyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacelytBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
